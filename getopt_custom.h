7369c3edc8cf438d1ff497843e6c0beb1b32790f
fadhil riyanto <me@fadev.org>
2025-06-21 22:26:24 +0700

    enhance docs
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- getopt_custom.h
+++ getopt_custom.h
@@ -1,9 +1,12 @@
 #ifndef GETOPT_CUSTOM_H
 #define GETOPT_CUSTOM_H 1
 
-#define GETOPT_CUSTOM_FILE              0x01
-#define GETOPT_CUSTOM_HEADER            0x02
-#define GETOPT_CUSTOM_HEADER_STRUCT     0x03 /* unused */
-#define GETOPT_CUSTOM_HEXDUMP           0x04
+#define GETOPT_CUSTOM_FILE              0x01 /* --file x */
+#define GETOPT_CUSTOM_HEADER            0x02 /* --header x, show user friendly ELF header */
+#define GETOPT_CUSTOM_HEADER_STRUCT     0x03 /* --struct-header x */
+                                             /* show ELF struct header that sync with linux kernel
+                                                struct flavor
+                                             */
+#define GETOPT_CUSTOM_HEXDUMP           0x04 /* hexdump all */
 
 #endif /* GETOPT_CUSTOM_H */
