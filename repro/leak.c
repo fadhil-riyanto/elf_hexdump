6015a9c1aba4b04530df53a6ad6c8421ee88626b
fadhil riyanto <me@fadev.org>
2025-07-12 17:49:41 +0700

    add test suite & update gitignore
    Signed-off-by: fadhil riyanto <me@fadev.org>

---
+++ repro/leak.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+        char* data = malloc(1024);
+        printf("%s", data);
+        return 0;
+}
---
+++ repro/rodata.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+        char *data = "abc";
+        printf("%s\n", data);
+        return 0;
+}
